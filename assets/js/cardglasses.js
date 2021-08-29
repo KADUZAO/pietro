03a56cddd2b094d0bf783fc9e1b5351b63216511
KADUZAO <71575086+KADUZAO@users.noreply.github.com>
2021-08-29 07:59:22 -0300

    Add files via upload

---
+++ assets/js/cardglasses.js
@@ -0,0 +1,13 @@
+VanillaTilt.init(document.querySelectorAll("#container-nine .card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+});
+
+VanillaTilt.init(document.querySelectorAll("#container-eleven .card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+});
